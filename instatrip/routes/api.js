baed22641b4b5f91c33a30852fb3050f570be6f2
Ryan Jones <ryan.j.jones10@gmail.com>
2015-07-16 12:43:18 -0700

    [feat] added getInstaData function

--- instatrip/routes/api.js
+++ instatrip/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var instagram = require('instagram');
-var gmaps = require('gmaps');
+// var instagram = require('../APIs/insta.js');
+// var gmaps = require('../APIs/gMaps.js');
 
 // GET map and photo data
 router.post('/', function(req, res, next) {
