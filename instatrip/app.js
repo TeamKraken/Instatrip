fefc57af362e555de1af413b9091195d407d1e90
Vincent Rupp Jr <vincent@vaerek.net>
2015-07-15 19:33:43 -0700

    [cleanup] removed/corrected boilerplate routing

--- instatrip/app.js
+++ instatrip/app.js
@@ -5,14 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/display');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// Route for initial form POST from landing page
+app.use('/display', display);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
