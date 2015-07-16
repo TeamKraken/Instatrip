d0574ade868a672fe3c57511eff884f626537061
Vincent Rupp Jr <vincent@vaerek.net>
2015-07-16 11:58:15 -0700

    [refactor] refactor api route to handle coordinate req

--- instatrip/routes/api.js
+++ instatrip/routes/api.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var instagram = require('instagram');
-var gmaps = require('gmaps');
+var instagram = require('instagram'); // TODO rename required file to match js file created by Ryan?
+var gmaps = require('gmaps'); // TODO rename required file to match js file created by James?
 
-// GET map and photo data
-router.post('/', function(req, res, next) {
-  res.send('JSON object');
+// GET photo data based on POSTed map coordinates
+router.post('/', function(req, res) {
+  // TODO refactor to properly access lat and long properties of req object
+  var lat = req.body.lat;
+  var long = req.body.long;
+
+  var photos = instagram(lat, long);
+  // TODO extract desired components of photos API object
+
+
+  res.send(photos);
 });
 
 module.exports = router;
