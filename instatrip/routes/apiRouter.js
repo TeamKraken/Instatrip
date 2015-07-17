db6b4d411418667bd95488033e104b47fa5b4ff9
Vincent Rupp Jr <vincent@vaerek.net>
2015-07-17 11:46:48 -0700

    [refactor] refactor apiRouter to receive map coords and invoke instagram api call for each coord set

--- instatrip/routes/apiRouter.js
+++ instatrip/routes/apiRouter.js
@@ -4,15 +4,18 @@ var router = express.Router();
 
 // GET photo data based on POSTed map coordinates
 router.post('/', function(req, res) {
-  // TODO extract map coordinates from req object
-  // var lat = req.body.lat;
-  // var long = req.body.long;
-  // var dist = 300; // Units: m, Max is 5000m
+  var photos = [];
+  var lat, lng, dist = 300; // dist unit: m, max: 5000m
 
-  var photos = obtainInstaData(lat, long, dist);
+  var coords = req.body.coords;
+  for (var i = 0; i < coords.length; i++){
+    lat = coords[i].lat;
+    lng = coords[i].lng;
+    photos.push(instagram.obtainInstaData(lat, long, dist));
+  }
 
   // Return photos object
-  // res.send(photos);
+  // res.send(JSON.stringify(photos));
 
   // For testing only:
   res.send('POST request received. Thanks David!');
