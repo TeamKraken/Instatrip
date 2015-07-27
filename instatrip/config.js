754d1b3e36b5eb854f45c81fee93c6aa442565a0
Ryan Jones <ryan.j.jones10@gmail.com>
2015-07-27 10:40:19 -0700

    [feat] add setInterval to keep dyno awake

--- instatrip/config.js
+++ instatrip/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  InstaClientID: 'ea354b6fc30e4b1180efe33ed233f7fe',
-  InstaClientSecret: '01397a2619064c898dc1ad0f72bc619e'
+  InstaClientID: '18151602fa7f402288bf8b9df11c301e',
+  InstaClientSecret: '1df4890556394feaac362e682274b92a'
 }
 
